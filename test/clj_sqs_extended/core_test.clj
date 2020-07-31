5bb2d2c1ccd44dc80705ce50cb0dfcb075ed60b7
daisybytes <daisybytes@mailbox.org>
2020-07-31 13:22:32 +0900

    Added wrappers to provide stanard and fifo queue access to the individual test functions.

--- test/clj_sqs_extended/core_test.clj
+++ test/clj_sqs_extended/core_test.clj
@@ -6,7 +6,7 @@
             [clj-sqs-extended.test-fixtures :as fixtures]))
 
 
-(use-fixtures :once fixtures/with-localstack-environment)
+(use-fixtures :once fixtures/with-test-ext-sqs-client)
 
 (defonce ^:private test-messages
   (into [] (take 5 (repeatedly helpers/random-message))))
@@ -16,49 +16,59 @@
 
 (deftest can-receive-message
   (testing "Sending/Receiving basic maps"
-    (let [client (fixtures/localstack-sqs)
-          url (fixtures/test-queue-url)
-          test-message (first test-messages)]
-      (doseq [format [:transit :json]]
-        (sqs-ext/send-message client url test-message {:format format})
-        (let [message (sqs-ext/receive-message client url {:format format})]
-          (is (= test-message (:body message))))))))
+    (fixtures/with-standard-queue
+      (let [test-message (first test-messages)]
+        (doseq [format [:transit :json]]
+          (sqs-ext/send-message @fixtures/test-ext-sqs-client
+                                @fixtures/test-standard-queue-url
+                                test-message {:format format})
+          (let [response (sqs-ext/receive-message @fixtures/test-ext-sqs-client
+                                                  @fixtures/test-standard-queue-url
+                                                  {:format format})]
+            (is (= test-message (:body response)))))))))
 
 (deftest can-auto-delete-message
   (testing "Auto-Deleting a single message after receiving it"
-    (let [client (fixtures/localstack-sqs)
-          url (fixtures/test-queue-url)
-          test-message (first test-messages)]
-      (sqs-ext/purge-queue client url)
-      (sqs-ext/send-message client url test-message)
-      (let [message (sqs-ext/receive-message client url {:auto-delete true})]
-        (is (= test-message (:body message)))
-        (is (= 0 (helpers/get-total-message-amount-in-queue client url)))))))
+    (fixtures/with-standard-queue
+      (let [test-message (first test-messages)]
+        (sqs-ext/purge-queue @fixtures/test-ext-sqs-client
+                             @fixtures/test-standard-queue-url)
+        (sqs-ext/send-message @fixtures/test-ext-sqs-client
+                              @fixtures/test-standard-queue-url
+                              test-message)
+        (let [response (sqs-ext/receive-message @fixtures/test-ext-sqs-client
+                                                @fixtures/test-standard-queue-url
+                                                {:auto-delete true})]
+          (is (= test-message (:body response)))
+          (is (= 0 (helpers/get-total-message-amount-in-queue @fixtures/test-ext-sqs-client
+                                                              @fixtures/test-standard-queue-url))))))))
 
 (deftest can-receive-fifo-messages
   (testing "Receiving multiple messages from FIFO queue in correct order"
-    (let [client (fixtures/localstack-sqs)
-          url (fixtures/test-fifo-queue-url)]
+    (fixtures/with-fifo-queue
       (doseq [format [:transit :json]]
         (doseq [test-message test-messages]
-          (sqs-ext/send-fifo-message client
-                                     url
+          (sqs-ext/send-fifo-message @fixtures/test-ext-sqs-client
+                                     @fixtures/test-fifo-queue-url
                                      test-message
                                      (helpers/random-group-id)
                                      {:format format}))
         (doseq [test-message test-messages]
-          (let [response (sqs-ext/receive-message client url {:format format})]
+          (let [response (sqs-ext/receive-message @fixtures/test-ext-sqs-client
+                                                  @fixtures/test-fifo-queue-url
+                                                  {:format format})]
             (is (= test-message (:body response)))))))))
 
 (deftest can-send-message-larger-than-256kb
   (testing "Sending a message with more than 256kb of data (via S3 bucket) in raw format"
-    (let [client (fixtures/localstack-sqs)
-          url (fixtures/test-queue-url)]
-      (sqs-ext/send-message client
-                            url
+    (fixtures/with-standard-queue
+      (sqs-ext/send-message @fixtures/test-ext-sqs-client
+                            @fixtures/test-standard-queue-url
                             test-message-larger-than-256kb
                             {:format :raw})
-      (let [message (sqs-ext/receive-message client url {:format :raw})
+      (let [response (sqs-ext/receive-message @fixtures/test-ext-sqs-client
+                                              @fixtures/test-standard-queue-url
+                                              {:format :raw})
             desired-length (count test-message-larger-than-256kb)
-            response-length (->> (:body message) (count))]
+            response-length (->> (:body response) (count))]
         (is (= desired-length response-length))))))
