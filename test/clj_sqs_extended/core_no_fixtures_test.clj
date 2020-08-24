08d075986e103d18b811fec993c4cb0b786ab827
daisybytes <daisybytes@mailbox.org>
2020-08-24 15:22:43 +0900

    This fixes the circleci tests.

--- test/clj_sqs_extended/core_no_fixtures_test.clj
+++ test/clj_sqs_extended/core_no_fixtures_test.clj
@@ -12,15 +12,17 @@
             SdkClientException]))
 
 
-(def ^:private aws-down-config {:access-key   (env :aws-access-key-id)
-                                :secret-key   (env :aws-secret-access-key)
-                                :endpoint-url "https://currently-unreachable-aws-endpoint.com"
-                                :region       (env :aws-sqs-region)})
+(def ^:private aws-down-config {:aws-access-key-id     (env :aws-access-key-id)
+                                :aws-secret-access-key (env :aws-secret-access-key)
+                                :aws-s3-endpoint-url   "https://currently-unreachable-aws-endpoint.com"
+                                :aws-sqs-endpoint-url  "https://currently-unreachable-aws-endpoint.com"
+                                :aws-region            (env :aws-region)})
 
-(def ^:private aws-config {:access-key   (env :aws-access-key-id)
-                           :secret-key   (env :aws-secret-access-key)
-                           :endpoint-url (env :aws-sqs-endpoint-url)
-                           :region       (env :aws-sqs-region)})
+(def ^:private aws-up-config {:aws-access-key-id     (env :aws-access-key-id)
+                              :aws-secret-access-key (env :aws-secret-access-key)
+                              :aws-s3-endpoint-url   (env :aws-s3-endpoint-url)
+                              :aws-sqs-endpoint-url  (env :aws-sqs-endpoint-url)
+                              :aws-region            (env :aws-region)})
 
 (deftest unreachable-endpoint-yields-proper-exception
   (testing "Trying to connect to an unreachable endpoint yields a proper exception"
@@ -35,7 +37,7 @@
 
 (deftest cannot-send-to-non-existing-bucket
   (testing "Sending a large message using a non-existing S3 bucket yields proper exception"
-    (let [sqs-ext-client (sqs/sqs-ext-client aws-config
+    (let [sqs-ext-client (sqs/sqs-ext-client aws-up-config
                                              "non-existing-bucket")]
       ;; WATCHOUT: Apparently the SDK does not verify the existance of the used S3
       ;;           bucket when the client is in instantiated so we need to send a
