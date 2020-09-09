e23306530ada31486eac5bb149383daaf8f66a06
daisybytes <daisybytes@mailbox.org>
2020-09-09 11:54:56 +0900

    Removed macro from sqs ns

--- src/clj_sqs_extended/aws/sqs.clj
+++ src/clj_sqs_extended/aws/sqs.clj
@@ -16,15 +16,6 @@
             SendMessageRequest]))
 
 
-(defmacro macro-name-tbd
-  [fn-name sqs-fn]
-  `(let [meta# (meta (var ~sqs-fn))]
-     (def ~fn-name
-       #(apply ~sqs-fn
-              (clj-sqs-extended.aws.sqs/sqs-ext-client %1)
-              %&))
-     (alter-meta! (var ~fn-name) merge meta#)))
-
 (defn sqs-ext-client
   [sqs-ext-config]
   (let [endpoint (aws/configure-sqs-endpoint sqs-ext-config)
