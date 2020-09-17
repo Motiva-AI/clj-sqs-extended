657bbbcfc29ce791527674977962f1107fa01210
Paul Lam <paul@quantisan.com>
2020-09-17 11:15:26 +0900

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject motiva/clj-sqs-extended "0.1.2"
+(defproject motiva/clj-sqs-extended "0.2.0-SNAPSHOT"
   :description "Clojure wrapper for https://github.com/awslabs/amazon-sqs-java-extended-client-lib"
   :url "https://github.com/Motiva-AI/clj-sqs-extended"
 
