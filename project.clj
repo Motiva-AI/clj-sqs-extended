8ef88e6526eee449bde5b6688314cc77dcb5571a
Paul Lam <paul@quantisan.com>
2020-10-29 16:53:46 +0900

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject motiva/clj-sqs-extended "0.3.0-SNAPSHOT"
+(defproject motiva/clj-sqs-extended "0.2.2"
   :description "Clojure wrapper for https://github.com/awslabs/amazon-sqs-java-extended-client-lib"
   :url "https://github.com/Motiva-AI/clj-sqs-extended"
 
