f5975d2a43249c20fc8ab4ea40f3147ac2f650ad
Paul Lam <paul@quantisan.com>
2021-02-21 09:07:27 +0900

    move only-acknowledge-messages-that-handler-is-available-to-process functional test to core-test ns

--- test/clj_sqs_extended/core_test.clj
+++ test/clj_sqs_extended/core_test.clj
@@ -427,6 +427,60 @@
            ;; teardown
            (stop-fn))))))
 
+(deftest ^:functional only-acknowledge-messages-that-handler-is-available-to-process
+  (let [n        5
+        messages (into [] (take n (repeatedly helpers/random-message-basic)))
+        c        (chan)
+
+        ;; setup
+        stop-receive-loop
+        (receive/receive-loop
+          @fixtures/test-queue-url
+          c
+          (partial sqs/receive-messages
+                   @fixtures/test-sqs-ext-client
+                   @fixtures/test-queue-url
+                   {:max-number-of-receiving-messages 1
+                    :wait-time-in-seconds             1})
+          (partial sqs/delete-message!
+                   @fixtures/test-sqs-ext-client
+                   @fixtures/test-queue-url)
+          {:auto-delete? false})]
+
+    (is (fn? stop-receive-loop))
+    (is (= {"ApproximateNumberOfMessages" 0, "ApproximateNumberOfMessagesNotVisible" 0}
+           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
+
+    (doseq [msg messages]
+      (sqs/send-message @fixtures/test-sqs-ext-client @fixtures/test-queue-url msg))
+
+    ;; these expected values require some explanation:
+    ;; 1. one message is read off the queue and become NotVisible
+    ;; 2. this first message is pushed to the output channel. Now the output
+    ;;    channel (default size is 1) is full.
+    ;; 3. on the next receive-loop iteration, a second message is read off the queue
+    ;; 4. but when receive-to-channel tries to put this second message to the
+    ;;    output channel, it is blocked because the output channel is full.
+    ;; 5. Thus we expect two messages to be read from the queue with
+    ;;    NotVisible values = 2
+    (is (= {"ApproximateNumberOfMessages" (- n 2), "ApproximateNumberOfMessagesNotVisible" 2}
+           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
+
+    (let [[out _] (alts!! [c (timeout 1000)])]
+      (is (:body out))
+      (is ((:done-fn out))))
+    (Thread/sleep 100) ;; wait for message to be deleted
+
+    (is (= {"ApproximateNumberOfMessages" (- n 3) , "ApproximateNumberOfMessagesNotVisible" 2}
+           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
+
+    ;; teardown
+    (stop-receive-loop)
+    (close! c)
+
+    ;; gives time for the receive-loop to stop
+    (Thread/sleep 500)))
+
 (deftest unreachable-endpoint-yields-proper-exception
   (let [unreachable-sqs-ext-client (sqs-ext/sqs-ext-client
                                      (merge fixtures/sqs-ext-config
--- test/clj_sqs_extended/internal/receive_test.clj
+++ test/clj_sqs_extended/internal/receive_test.clj
@@ -1,5 +1,5 @@
 (ns clj-sqs-extended.internal.receive-test
-  (:require [clojure.test :refer [deftest is use-fixtures]]
+  (:require [clojure.test :refer [deftest is]]
             [bond.james :as bond]
             [clojure.core.async :as async :refer [chan close! timeout alts!! <!!]]
             [clojure.core.async.impl.protocols :as async-protocols]
@@ -8,9 +8,6 @@
             [clj-sqs-extended.test-helpers :as helpers]
             [clj-sqs-extended.aws.sqs :as sqs]
             [clj-sqs-extended.internal.receive :as receive]))
-
-(use-fixtures :once fixtures/with-test-sqs-ext-client fixtures/with-test-s3-bucket)
-(use-fixtures :each fixtures/with-transient-queue)
 
 (def mock-received-messages [{:receiptHandle "receipt"
                               :body          "foo"}])
@@ -64,61 +61,6 @@
 
   ;; gives time for the receive-loop to stop. otherwise, subsequent tests might fail
   (Thread/sleep 300))
-
-(deftest only-acknowledge-messages-that-handler-is-available-to-process
-  (let [n        5
-        messages (into [] (take n (repeatedly helpers/random-message-basic)))
-        c        (chan)
-
-        ;; setup
-        stop-receive-loop
-        (receive/receive-loop
-          @fixtures/test-queue-url
-          c
-          (partial sqs/receive-messages
-                   @fixtures/test-sqs-ext-client
-                   @fixtures/test-queue-url
-                   {:max-number-of-receiving-messages 1
-                    :wait-time-in-seconds             1})
-          (partial sqs/delete-message!
-                   @fixtures/test-sqs-ext-client
-                   @fixtures/test-queue-url)
-          {:auto-delete? false})]
-
-    (is (fn? stop-receive-loop))
-    (is (= {"ApproximateNumberOfMessages" 0, "ApproximateNumberOfMessagesNotVisible" 0}
-           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
-
-
-    (doseq [msg messages]
-      (sqs/send-message @fixtures/test-sqs-ext-client @fixtures/test-queue-url msg))
-
-    ;; these expected values require some explanation:
-    ;; 1. one message is read off the queue and become NotVisible
-    ;; 2. this first message is pushed to the output channel. Now the output
-    ;;    channel (default size is 1) is full.
-    ;; 3. on the next receive-loop iteration, a second message is read off the queue
-    ;; 4. but when receive-to-channel tries to put this second message to the
-    ;;    output channel, it is blocked because the output channel is full.
-    ;; 5. Thus we expect two messages to be read from the queue with
-    ;;    NotVisible values = 2
-    (is (= {"ApproximateNumberOfMessages" (- n 2), "ApproximateNumberOfMessagesNotVisible" 2}
-           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
-
-    (let [[out _] (alts!! [c (timeout 1000)])]
-      (is (:body out))
-      (is ((:done-fn out))))
-    (Thread/sleep 100) ;; wait for message to be deleted
-
-    (is (= {"ApproximateNumberOfMessages" (- n 3) , "ApproximateNumberOfMessagesNotVisible" 2}
-           (sqs/queue-attributes @fixtures/test-sqs-ext-client @fixtures/test-queue-url)))
-
-    ;; teardown
-    (stop-receive-loop)
-    (close! c)
-
-    ;; gives time for the receive-loop to stop
-    (Thread/sleep 500)))
 
 (defn- mock-receiver-fn [] [:foo])
 
