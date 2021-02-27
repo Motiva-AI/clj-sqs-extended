b3d4b15b61e62d4a73a3df363d8232288c5b9d07
Paul Lam <paul@quantisan.com>
2021-02-27 20:30:40 +0900

    clojure.tools.namespace.repl/set-refresh-dirs in user ns

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -2,3 +2,8 @@
   (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [circleci.test]))
 
+;; Hotfix for https://github.com/clojure-emacs/orchard/issues/103 and
+;; https://github.com/clojure-emacs/cider/issues/2686
+;; Do not try to load source code from 'resources' directory
+(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
+
